6be369c90d977bbdbbfcb25fcb8831c83b370431
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-11-20 22:13:53 -0700

    Create search form

--- public/js/app.js
+++ public/js/app.js
@@ -11,3 +11,12 @@ fetch('http://localhost:3000/weather?address=boston').then((response) => {
         }
     })
 })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+})
