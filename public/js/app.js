e3248a726380f08ee6dc8f40378940040609f436
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-11-21 22:25:04 -0700

    Fetch forecast for user provided location

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,6 @@
 // Client side javascript that will run in the browser
 console.log('Client side javascript file loaded')
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    resonpse.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
@@ -19,4 +8,17 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        resonpse.json().then((data) => {
+            if(data.error){
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+
+    
 })
